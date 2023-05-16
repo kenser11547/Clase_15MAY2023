0c1eff84658002df92f77be4ee2fce98ead45946
kenser <koffy140221@gmail.com>
2023-05-15 20:43:14 -0600

    resolucion final

--- Herencias_poliformismo_tarea.php
+++ Herencias_poliformismo_tarea.php
@@ -10,23 +10,20 @@ error_reporting(E_ALL);
 abstract class Departamentos{
     protected $municipios;
     public $habitantes;
-    public static $nacionalidad = "guatemaltecos";
     private $informacion;
-
-    public function __construct($municipios, $habitantes){
+    public static $nacionalidad = "guatemaltecos";
+    
+    public function __construct(int $municipios = 0, int $habitantes = 0, string $informacion = "confidencial"){
         $this->municipios = $municipios;
         $this->habitantes = $habitantes;
-    }
-    private function metodoPrivado () : void {
-        $this->informacion = "confidencial";
-    }
-    public function accederPrivado() : void {
-        echo $this->informacion;
-        $this->metodoPrivado();
-    }
+        $this->informacion = $informacion;
 
+    }
     final public static function getNacionalidad(){
         echo "TODAS LAS PERSONAS SON DE ORIGEN:" . self::$nacionalidad;
+    }
+    private function getInfor() : void {
+        $this->informacion;
     }
 
     public function getInfo() : void{
@@ -36,6 +33,9 @@ abstract class Departamentos{
     public function getMunicipios(): void{
         echo "El departamento tiene: $this->municipios municipios";
     }
+    public function getHabitantes(): void{
+        echo "El departamento tiene un aproximado de: $this->habitantes habitantes";
+    }
 }
 #asignar los hijos/clases
 class AltaVerapaz extends Departamentos{
@@ -44,7 +44,7 @@ class AltaVerapaz extends Departamentos{
     public function __construct($idiomas = 0, $municipios = 0, $habitantes = 0 , $informacion = 'confidencial')
     {
         $this->idiomas = $idiomas;
-        parent::__construct($municipios, $habitantes);
+        parent::__construct($municipios, $habitantes, $informacion);
     }
 
     public function getInfo() : void{
@@ -53,14 +53,20 @@ class AltaVerapaz extends Departamentos{
 }
 
 class Guatemala extends Departamentos{
+
+    public $cultura;
+    public function __construct($cultura = "sin cultura", $municipios = 0, $habitantes = 0 , $informacion = 'confidencial'){
+        $this->cultura = $cultura;
+        parent::__construct($municipios, $habitantes, $informacion);
+    }
     public function getInfo() : void{
-        echo "Guatemala tiene $this->municipios municipios, con un aproximado de: $this->habitantes de habitantes";
+        echo "Guatemala tiene $this->municipios municipios, con un aproximado de: $this->habitantes de habitantes y $this->cultura en su cultura y son ". self::$nacionalidad; ;
     }
 }
 
-$AltaVerapazCity = new AltaVerapaz(4,16,150000,);
+$AltaVerapazCity = new AltaVerapaz(4,16,150000);
 //impresion de las clases.
 $AltaVerapazCity->getInfo();
 echo "<br>";
-$GuatemalaCity = new Guatemala(10,1000000);
+$GuatemalaCity = new Guatemala("pluricultura",10,1000000);
 $GuatemalaCity->getInfo();
